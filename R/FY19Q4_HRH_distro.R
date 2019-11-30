1aa37b909248247e9be709051b3c5c78ca87d21e
Aaron Chafetz <achafetz@usaid.gov>
2019-11-30 09:56:46 -0500

    add R scripts from misc_scripts related to self assessments

---
+++ R/FY19Q4_HRH_distro.R
@@ -0,0 +1,133 @@
+## PROJECT:  AGENCY SELF ASSESSMENT
+## AUTHOR:   A.CHAFETZ | USAID
+## PURPOSE:  review HRH data
+## DATE:     2019-11-27
+## UPDATED:  
+
+#dependencies
+  library(tidyverse)
+  library(ICPIutilities)
+  library(scales)
+  library(extrafont)
+  library(gridExtra)
+
+#import
+  path <- "C:/Users/achafetz/Downloads/PEPFAR-Data-Genie-SiteByIMs-2019-11-29.zip"
+  df <- read_msd(path)
+  
+#select country
+  ou_sel <- "Malawi"
+  
+#subset to HRH and TX_CURR and OU
+  df_hrh <- df %>% 
+    filter(operatingunit == ou_sel,
+           sitetype %in% c("Facility", "Community"),
+           fundingagency == "USAID",
+           indicator %in% c("TX_CURR", "HRH_CURR"),
+           standardizeddisaggregate %in% c("Total Numerator", "CadreCategory"),
+           fiscal_year == 2019)
+  
+#adjust geographic focus
+  df_hrh <- df_hrh %>% 
+    mutate(snu_lvl = case_when(operatingunit == "Malawi" ~ psnu,
+                              TRUE ~ snu1))
+  
+#aggregate
+  df_hrh <- df_hrh %>% 
+    group_by(operatingunit, snu_lvl, indicator, standardizeddisaggregate, otherdisaggregate) %>% 
+    summarise_at(vars(cumulative), sum, na.rm = TRUE) %>% 
+    ungroup()
+  
+#adjust names
+  df_hrh <- df_hrh %>% 
+    mutate(snu_lvl = str_remove(snu_lvl, " District"))
+
+#HRH share
+  df_hrh_share <- df_hrh %>% 
+    filter(standardizeddisaggregate == "Total Numerator") %>% 
+    group_by(operatingunit, indicator) %>% 
+    mutate(reg_share = cumulative / sum(cumulative, na.rm = TRUE)) %>% 
+    ungroup()
+
+  (viz_share <- df_hrh_share %>% 
+    select(-cumulative) %>% 
+    spread(indicator, reg_share) %>% 
+    filter(!is.na(HRH_CURR)) %>% 
+    mutate(TX_CURR = ifelse(is.na(TX_CURR), 0, TX_CURR),
+           snu_lvl = fct_reorder(snu_lvl, TX_CURR)) %>% 
+    gather(indicator, reg_share, HRH_CURR, TX_CURR) %>% 
+    arrange(snu_lvl, indicator) %>% 
+    mutate(lab = case_when(snu_lvl %in% c("Iringa", "Mulanje") ~ indicator)) %>% 
+    ggplot(aes(reg_share, snu_lvl, group = snu_lvl, color = indicator)) +
+    geom_vline(xintercept = 0, color = "gray50") +
+    geom_path(color = "gray70", size = 1, na.rm = TRUE) +
+    geom_point(size = 6.5, na.rm = TRUE) +
+    geom_text(aes(label = lab, 
+                  hjust = ifelse(indicator == "TX_CURR", -.3, 1.2)), 
+              family = "Gill Sans MT", fontface = "bold", 
+              size = 3, na.rm = TRUE) +
+    scale_x_continuous(labels = percent_format(1), expand = c(0.005, 0.005)) +
+    scale_color_manual(values = c("#26456a", "gray60")) +
+    labs(x = NULL, y = NULL, 
+         subtitle = "Regional Share of HRH/Treatment",
+         caption = " ") +
+    expand_limits(x = .2) +
+    facet_wrap(~ operatingunit) +
+    theme_minimal() +
+    theme(axis.ticks = element_blank(),
+          text = element_text(family = "Gill Sans MT", size = 14),
+          strip.text = element_text(color = "white"),
+          legend.position = "none"
+          ))
+  
+#HRH Cadre
+  tx_order <- df_hrh %>% 
+    filter(indicator == "TX_CURR") %>% 
+    count(snu_lvl, wt = cumulative) %>% 
+    arrange(n) %>% 
+    pull(snu_lvl)
+  
+  if(ou_sel == "Malawi")
+    tx_order <- c("Chiradzulu", "Thyolo", tx_order)
+  
+ df_viz_cadre <- df_hrh %>% 
+    filter(standardizeddisaggregate == "CadreCategory") %>% 
+    mutate(otherdisaggregate = fct_lump(otherdisaggregate, 3, w = cumulative, other_level = "All Other Cadres")) %>% 
+    group_by(snu_lvl, otherdisaggregate) %>% 
+    summarise_at(vars(cumulative), sum, na.rm = TRUE) %>% 
+    ungroup() %>%
+    group_by(snu_lvl) %>% 
+    mutate(cadre_share = cumulative/sum(cumulative, na.rm = TRUE)) %>% 
+    ungroup() %>% 
+    mutate(snu_lvl = factor(snu_lvl, tx_order),
+           otherdisaggregate = factor(otherdisaggregate, c("Lay", "Clinical", "Social Service", "All Other Cadres"))
+           )
+ 
+ (viz_cadre <- df_viz_cadre %>% 
+    ggplot(aes(snu_lvl, cadre_share, fill = otherdisaggregate)) +
+    geom_col(na.rm = TRUE) +
+    geom_blank(aes(y = 1.2 * cadre_share), na.rm = TRUE) +
+    geom_hline(yintercept = 0, color = "gray50") +
+    geom_text(aes(label = percent(cadre_share, 1)), hjust = -.2,
+              color = "gray30", family = "Gill Sans MT", na.rm = TRUE) +
+    scale_y_continuous(labels = percent) +
+    coord_flip() +
+    scale_fill_manual(values = c("#335B8E", "#739bcc","#739bcc", "#D0DEEF")) +
+    facet_wrap(. ~ otherdisaggregate, nrow = 1) +
+    labs(x = NULL, y = NULL, 
+         subtitle = "Main HRH Cadres",
+         caption = "Source: FY19Q4i MSD") +
+    theme_minimal() +
+    theme(legend.position = "none",
+          text = element_text(family = "Gill Sans MT", size = 14),
+          axis.text.x = element_text(color = "white"),
+          axis.text.y = element_blank(),
+          panel.grid.major = element_blank(),
+          panel.grid.minor = element_blank(),
+          plot.caption = element_text(color = "gray30", size = 10)))
+
+ viz_out <- grid.arrange(viz_share, viz_cadre, nrow = 1)
+
+ ggsave("../Downloads/MWI_HRH_Distro.png", viz_out, dpi = 300,
+        height = 5.6, width = 10, units = "in")     
+  
---
+++ R/FY19Q4_TZA_TX_Comparison.R
@@ -0,0 +1,196 @@
+## PROJECT:  AGENCY SELF ASSESSMENT
+## AUTHOR:   A.CHAFETZ | USAID
+## PURPOSE:  Visualize trends for the 90s indicators
+## DATE:     2019-11-19
+## UPDATED:  
+
+#dependencies
+
+library(tidyverse)
+library(ICPIutilities)
+library(scales)
+library(extrafont)
+library(gridExtra)
+
+path_msd <- list.files("ICPI/Data", "OU_IM", full.names = TRUE)
+
+path_genie <- "../Downloads/PEPFAR-Data-Genie-OUByIMs-2019-11-19/Genie_Daily_c2a76a83ee734074819d3863be59a08d.txt"
+
+df_msd <- read_rds(path_msd)
+
+df_genie <- read_tsv(path_genie, col_types = c(.default = "c")) %>% 
+  select(-c("dataelementuid", "approvallevel", "approvalleveldescription", 
+            "categoryoptioncombouid")) %>% 
+  mutate(fiscal_year = as.integer(fiscal_year)) %>% 
+  mutate_at(vars(targets:cumulative), as.double)
+
+df_tx <- df_msd %>% 
+  filter(fiscal_year != 2019,
+         indicator %in% c("TX_CURR", "TX_NEW", "TX_NET_NEW"),
+         standardizeddisaggregate == "Total Numerator")
+
+df_tx_fy19q4 <- df_genie %>% 
+  filter(fiscal_year == 2019,
+         indicator %in% c("TX_CURR", "TX_NEW", "TX_NET_NEW"),
+         standardizeddisaggregate == "Total Numerator")
+
+df_tx_combo <- bind_rows(df_tx, df_tx_fy19q4) 
+
+df_tx_combo <- df_tx_combo %>% 
+  filter(fundingagency == "USAID",
+         operatingunit == "Tanzania")
+
+df_tx_combo <- df_tx_combo %>% 
+  group_by(indicator, fundingagency, fiscal_year) %>% 
+  summarise_if(is.numeric, sum, na.rm = TRUE) %>% 
+  ungroup() %>% 
+  reshape_msd("long") %>% 
+  filter(str_detect(period, "q")) %>% 
+  arrange(indicator, period) 
+
+# df_tx_combo <- df_tx_combo %>% 
+#   spread(indicator, val) %>% 
+#   mutate(`Proxy Retention` = 1 + (TX_NET_NEW - TX_NEW)/(TX_CURR - TX_NET_NEW + TX_NEW)) %>% 
+#   gather(indicator, val,-period)
+
+
+df_viz <- df_tx_combo %>% 
+  mutate(period = str_remove(period, "20") %>% toupper,
+         pd_fill = case_when(period == "FY19Q4" ~ "z_now",
+                             str_detect(period, "19") ~ "y_rest of fy19",
+                             TRUE ~ "x_earlier"))
+
+
+v_curr <- df_viz %>% 
+  filter(indicator == "TX_CURR") %>% 
+  ggplot(aes(period, val, fill = pd_fill)) +
+  geom_col() +
+  #geom_text(aes(label = round(val/1000000, 2) %>% paste0("m")),
+  geom_text(aes(label = round(val/1000, 0) %>% paste0("k")),
+            family = "Gill Sans MT", color = "gray30", size = 3,
+            vjust = -1) +
+  geom_hline(yintercept = 0) +
+  labs(x = NULL, y = NULL, caption = "") +
+  scale_fill_manual(values = c("#739bcc", "#335B8E", "#26456a")) +
+  scale_y_continuous(labels = comma) +
+  scale_x_discrete(labels = c("FY17Q1", "", "FY17Q3", "",
+                              "FY18Q1", "", "FY18Q3", "",
+                              "FY19Q1", "", "FY19Q3", "")) +
+  facet_wrap(indicator ~ ., scales = "free_y") +
+  theme_light() +
+  theme(axis.ticks = element_blank(),
+        text = element_text(family = "Gill Sans MT"),
+        axis.text.y = element_blank(),
+        panel.grid.major.x = element_blank(),
+        strip.text = element_text(size = 16),
+        legend.position = "none")
+
+v_new <- df_viz %>% 
+  filter(indicator %in% c("TX_NEW", "TX_NET_NEW")) %>% 
+  ggplot(aes(period, val, fill = pd_fill)) +
+  geom_col() +
+  geom_blank(aes(y = 1.2 * val)) +
+  geom_text(aes(label = round(val/1000, 0) %>% paste0("k"),
+                vjust = ifelse(val <0, 1, -.8)), size = 3,
+            family = "Gill Sans MT", color = "gray30") +
+  geom_hline(yintercept = 0) +
+  labs(x = NULL, y = NULL, caption = "Source: FY19Q4 Genie Pull [2019-11-19]") +
+  scale_fill_manual(values =  c("#739bcc", "#335B8E", "#26456a")) +
+  scale_y_continuous(labels = comma)+
+  scale_x_discrete(labels = c("FY17Q1", "", "FY17Q3", "",
+                              "FY18Q1", "", "FY18Q3", "",
+                              "FY19Q1", "", "FY19Q3", "")) +
+  facet_wrap(. ~ indicator, ncol = 1) +
+  theme_light() +
+  theme(axis.ticks = element_blank(),
+        text = element_text(family = "Gill Sans MT"),
+        axis.text.y = element_blank(),
+        panel.grid.major.x = element_blank(),
+        strip.text = element_text(size = 16),
+        plot.caption = element_text(size = 9, color = "gray30"),
+        legend.position = "none")
+
+(v_out <- grid.arrange(v_curr, v_new, nrow = 1))
+
+# ggsave("TX_NET_NEW_USAID_Trends.png", plot = v_out, path = "../Downloads",
+#        height = 5.63, width = 10, units = "in", dpi = 300)
+
+ggsave("TX_NET_NEW_TZA_USAID_Trends.png", plot = v_out, path = "../Downloads",
+       height = 5.63, width = 10, units = "in", dpi = 300)
+
+#Ou breakdown
+
+df_ou <- df_tx_combo %>% 
+  filter(fundingagency == "USAID") %>% 
+  group_by(indicator, operatingunit, fiscal_year) %>% 
+  summarise_if(is.numeric, sum, na.rm = TRUE) %>% 
+  ungroup() %>% 
+  reshape_msd("long") %>% 
+  filter(str_detect(period, "q")) %>% 
+  arrange(operatingunit, indicator, period)
+
+df_ou_gr <- df_ou %>% 
+  filter(indicator == "TX_CURR") %>% 
+  group_by(operatingunit) %>% 
+  mutate(delta = val - lag(val),
+         growth = (val - lag(val))/lag(val)) %>% 
+  filter(period == "fy2019q4")
+
+#New v Net NEW
+
+df_ratio <- df_tx_combo %>% 
+  filter(fundingagency == "USAID") %>% 
+  spread(indicator, val) %>% 
+  mutate(ratio = TX_NET_NEW/TX_NEW,
+         period = str_remove(period, "20") %>% toupper,
+         period = as_factor(period)) 
+
+df_ratio %>% 
+  ggplot(aes(TX_NEW, TX_NET_NEW)) +
+  geom_hline(yintercept = 0) +
+  geom_abline(slope = .5, intercept = 0) +
+  geom_path(size = 1) +
+  geom_point(size = 5) +
+  expand_limits(y = 0, x = 0) +
+  geom_text(aes(label = period),
+            family = "Gill Sans MT", color = "gray30",
+            vjust = -.8) +
+  scale_y_continuous(labels = comma) +
+  scale_x_continuous(labels = comma) +
+  theme_light() +
+  theme(text = element_text(family = "Gill Sans MT"))
+
+#funding agency
+df_tx_combo %>% 
+  filter(fundingagency %in% c("USAID", "HHS/CDC"),
+         indicator %in% c("TX_NET_NEW", "TX_CURR")) %>% 
+  mutate(period = str_remove(period, "20") %>% toupper,
+         fundingagency = str_remove(fundingagency, "HHS/"),
+         fundingagency = factor(fundingagency, c("USAID", "CDC"))) %>% 
+  ggplot(aes(period, val, fill = fundingagency)) +
+  geom_col() +
+  geom_blank(aes(y = 1.2 * val)) +
+  geom_text(aes(label = ifelse(val > 1000000, 
+                               round(val/1000000, 2) %>% paste0("m"),
+                               round(val/1000, 0) %>% paste0("k")),
+                vjust = ifelse(val <0, 1, -.8)), size = 3,
+            family = "Gill Sans MT", color = "gray30") +
+  geom_hline(yintercept = 0) +
+  labs(x = NULL, y = NULL, caption = "Source: FY19Q4 Genie Pull [2019-11-19]") +
+  scale_fill_manual(values =  c("#335B8E", "#739bcc")) +
+  scale_y_continuous(labels = comma)+
+  scale_x_discrete(labels = c("FY17Q1", "", "FY17Q3", "",
+                              "FY18Q1", "", "FY18Q3", "",
+                              "FY19Q1", "", "FY19Q3", "")) +
+  facet_grid(indicator ~ fundingagency, scales = "free_y") +
+  theme_light() +
+  theme(axis.ticks = element_blank(),
+        text = element_text(family = "Gill Sans MT"),
+        axis.text.y = element_blank(),
+        panel.grid.major.x = element_blank(),
+        strip.text = element_text(size = 16),
+        plot.caption = element_text(size = 9, color = "gray30"),
+        legend.position = "none")
+
+ggsave("TX_Agency_Comaparison.png", path = "../Downloads",
+       height = 5.63, width = 10, units = "in", dpi = 300)
